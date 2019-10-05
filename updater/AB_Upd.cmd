1187eb7e29390730e02609eba4f1258b08e4134e
eXP10iT <kron66@ya.ru>
2019-10-05 19:29:24 +0300

    Предрелиз Паковать в инсталлер только каталог updater

--- updater/AB_Upd.cmd
+++ updater/AB_Upd.cmd
@@ -2,16 +2,16 @@
 title A+B_Update
 cls
 
-https://github.com/eXP10iT6/AB_SQL.git
-
+cd c:\AB_SQL
+C:\AB_SQL\updater\PortableGit\cmd\git.exe checkout -- ./ && git pull
 
 rem Работа с каталогами
-md C:\AB_SQL\client
-md C:\AB_SQL\updater\UpdUpd
-del /F /Q C:\AB_SQL\client\*
-del /F /Q C:\AB_SQL\updater\UpdUpd\*
+rem md C:\AB_SQL\client
+rem md C:\AB_SQL\updater\UpdUpd
+rem del /F /Q C:\AB_SQL\client\*
+rem del /F /Q C:\AB_SQL\updater\UpdUpd\*
 
-C:\AB_SQL\updater\ftp -v -s:C:\AB_SQL\updater\answers.dat
+rem C:\AB_SQL\updater\ftp -v -s:C:\AB_SQL\updater\answers.dat
 
 rem Копируем ярлыки на рабочий стол
 copy "C:\AB_SQL\updater\UpdUpd\А+Б_Обновление.lnk" "%USERPROFILE%/Desktop/"
@@ -30,6 +30,6 @@ copy "C:\AB_SQL\updater\UpdUpd\AB_Upd.cmd" "C:/AB_SQL/updater/"
 copy "C:\AB_SQL\updater\UpdUpd\answers.dat" "C:/AB_SQL/updater/"
 copy "C:\AB_SQL\updater\UpdUpd\ftp.exe" "C:/AB_SQL/updater/"
 
-C:\AB_SQL\client\7z.exe e -oc:\ab_sql -y  C:\AB_SQL\client\BALANS.7z
+rem C:\AB_SQL\client\7z.exe e -oc:\ab_sql -y  C:\AB_SQL\client\BALANS.7z
 call c:\ab_sql\updater\check.exe
 rem pause

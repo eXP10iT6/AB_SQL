b3f9a0771fb3254394735565ff113214e7f10ff6
eXP10iT <kron66@ya.ru>
2019-10-06 15:06:23 +0300

    cleaning

--- updater/AB_Upd.cmd
+++ updater/AB_Upd.cmd
@@ -6,14 +6,6 @@ cd c:\AB_SQL
 "C:\AB_SQL\updater\PortableGit\bin\git.exe" clone --progress -v "https://github.com/eXP10iT6/AB_SQL.git" "C:\AB_SQL"
 "C:\AB_SQL\updater\PortableGit\bin\git.exe" checkout -- ./ && "C:\AB_SQL\updater\PortableGit\bin\git.exe" pull
 
-rem Работа с каталогами
-rem md C:\AB_SQL\client
-rem md C:\AB_SQL\updater\UpdUpd
-rem del /F /Q C:\AB_SQL\client\*
-rem del /F /Q C:\AB_SQL\updater\UpdUpd\*
-
-rem C:\AB_SQL\updater\ftp -v -s:C:\AB_SQL\updater\answers.dat
-
 rem Копируем ярлыки на рабочий стол
 copy "C:\AB_SQL\updater\UpdUpd\А+Б_Обновление.lnk" "%USERPROFILE%/Desktop/"
 copy "C:\AB_SQL\updater\UpdUpd\А+Б_Обновление.lnk" "%USERPROFILE%/Рабочий стол/"
@@ -27,10 +19,5 @@ copy "C:\AB_SQL\updater\UpdUpd\Ђ+Ѓ.lnk" "%USERPROFILE%/Desktop/"
 copy "C:\AB_SQL\updater\UpdUpd\Ђ+Ѓ.lnk" "%USERPROFILE%/ђ Ў®зЁ© бв®«/"
 cls
 
-rem copy "C:\AB_SQL\updater\UpdUpd\AB_Upd.cmd" "C:/AB_SQL/updater/"
-rem copy "C:\AB_SQL\updater\UpdUpd\answers.dat" "C:/AB_SQL/updater/"
-rem copy "C:\AB_SQL\updater\UpdUpd\ftp.exe" "C:/AB_SQL/updater/"
-
-rem C:\AB_SQL\client\7z.exe e -oc:\ab_sql -y  C:\AB_SQL\client\BALANS.7z
 call c:\ab_sql\updater\check.exe
 rem pause
